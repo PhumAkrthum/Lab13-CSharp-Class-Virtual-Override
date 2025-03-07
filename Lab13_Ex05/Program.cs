0ecac125922bbd811032b2eed79cae5261c3424c
PhumAkrthum <66030270@kmitl.ac.th>
2025-03-07 20:43:53 +0700

    Update4-7

---
+++ Lab13_Ex05/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+
+public abstract class Shape
+{
+    public abstract void Draw();
+}
+
+public class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a circle");
+    }
+}
+
+public class Rectangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a rectangle");
+    }
+}
+
+public class Triangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a triangle");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Shape> shapes = new List<Shape>
+        {
+            new Circle(),
+            new Rectangle(),
+            new Circle(),
+            new Triangle()
+        };
+
+        Console.WriteLine("Drawing Shapes:");
+        foreach (Shape shape in shapes)
+        {
+            shape.Draw();
+        }
+    }
+}
---
+++ Lab13_Ex06/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+public class Animal
+{
+    public string Name { get; set; }
+
+    public Animal(string name)
+    {
+        Name = name;
+        Console.WriteLine($"Animal Constructor: {Name}");
+    }
+}
+
+public class Dog : Animal
+{
+    public string Breed { get; set; }
+
+    public Dog(string name, string breed) : base(name)
+    {
+        Breed = breed;
+        Console.WriteLine($"Dog Constructor: {Name}, {Breed}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dog dog = new Dog("Buddy", "Golden Retriever");
+
+       
+        Console.WriteLine($"Dog Name: {dog.Name}");
+        Console.WriteLine($"Dog Breed: {dog.Breed}");
+    }
+}
