aa4b2fd079dd31874d9a76dceeb6b9f496456d9e
PhumAkrthum <66030270@kmitl.ac.th>
2025-03-07 20:04:27 +0700

    Update1-3

---
+++ Lab13_Ex03/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Animal makes a sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Cat meows");
+    }
+}
+
+public class AnimalSound
+{
+    public static void PlaySound(Animal animal)
+    {
+        animal.MakeSound();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        AnimalSound.PlaySound(dog);
+        AnimalSound.PlaySound(cat);
+    }
+}
